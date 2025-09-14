92d71a851d7fcde2f8742ec97adfa8013fb08f33
Srinivas Koruprolu <srinivaskoruprolu5@gmail.com>
2025-09-14 06:34:12 +0000

    fix the login issue and navigation issues by full diagnosis

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBgBHPETXpnaZnNryHakqlxv7cfGdOq3qs",
-  authDomain: "encoded-net-463504-a2.firebaseapp.com",
-  projectId: "encoded-net-463504-a2",
-  storageBucket: "encoded-net-463504-a2.appspot.com",
-  messagingSenderId: "86065515836",
-  appId: "1:86065515836:web:141d66a64cb78844416239",
-  measurementId: "G-RDMJKRBPDZ"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 
