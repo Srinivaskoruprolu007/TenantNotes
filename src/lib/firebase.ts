f60ecd76f6bea26a1ab5bd9373ef62a5d528e157
Srinivas Koruprolu <srinivaskoruprolu5@gmail.com>
2025-09-14 05:54:24 +0000

    still it's not working and getting an error like Authentication Failes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,15 +3,17 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBgBHPETXpnaZnNryHakqlxv7cfGdOq3qs",
   authDomain: "encoded-net-463504-a2.firebaseapp.com",
   projectId: "encoded-net-463504-a2",
-  storageBucket: "encoded-net-463504-a2.appspot.com",
+  storageBucket: "encoded-net-463504-a2.firebasestorage.app",
   messagingSenderId: "86065515836",
   appId: "1:86065515836:web:141d66a64cb78844416239",
   measurementId: "G-RDMJKRBPDZ"
 };
+
 
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
